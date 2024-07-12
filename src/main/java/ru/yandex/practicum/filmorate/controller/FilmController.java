dda543ef04043a10d4b74e35c47e2f93b63ef943
Елистратов <elistratov-av@yandex.ru>
2024-07-12 13:37:51 +0300

    10 спринт: создать rest-сервис с пользователями и фильмами

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,85 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        // проверяем выполнение необходимых условий
+        validate(film, false);
+        // формируем дополнительные данные
+        film.setId(getNextId());
+        // сохраняем новую публикацию в памяти приложения
+        films.put(film.getId(), film);
+        log.info("Создан фильм: {}", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film newFilm) {
+        // проверяем необходимые условия
+        validate(newFilm, true);
+        Film oldFilm = films.get(newFilm.getId());
+        if (oldFilm != null) {
+            // если фильм найден и все условия соблюдены, обновляем его содержимое
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(newFilm.getDuration());
+            log.info("Изменен фильм: {}", oldFilm);
+            return oldFilm;
+        }
+        log.warn("Фильм с id = {} не найден", newFilm.getId());
+        throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
+    }
+
+    // вспомогательный метод для генерации идентификатора нового фильма
+    private int getNextId() {
+        int currentMaxId = films.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    private static final LocalDate MOVIE_BIRTHDAY = LocalDate.of(1895, Month.DECEMBER, 28);
+
+    private static void validate(Film film, final boolean checkId) {
+        try {
+            if (checkId && film.getId() == null)
+                throw new ValidationException("Id должен быть указан");
+            if (film.getReleaseDate().isBefore(MOVIE_BIRTHDAY))
+                throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+        } catch (ValidationException e) {
+            log.warn("Ошибка валидации фильма: {}", e.getMessage());
+            throw e;
+        }
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,24 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-public class Film {
+public class User {
     private Integer id;
+    @Email
+    private String email;
     @NotBlank
+    private String login;
     private String name;
-    @Size(max = 200)
-    private String description;
-    @NotNull
+    @PastOrPresent
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalDate releaseDate;
-    @Positive
-    private double duration;
+    private LocalDate birthday;
 }
