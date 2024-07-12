e48482630df213c4d79748d9fe60178c27bcfdc7
Елистратов <elistratov-av@yandex.ru>
2024-07-12 13:37:51 +0300

    10 спринт: создать rest-сервис с пользователями и фильмами

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,84 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        // проверяем выполнение необходимых условий
+        validate(user, false);
+        // формируем дополнительные данные
+        user.setId(getNextId());
+        // сохраняем нового пользователя в памяти приложения
+        users.put(user.getId(), user);
+        log.info("Создан пользователь: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User newUser) {
+        // проверяем необходимые условия
+        validate(newUser, true);
+        User oldUser = users.get(newUser.getId());
+        if (oldUser != null) {
+            // если фильм найден и все условия соблюдены, обновляем его содержимое
+            oldUser.setEmail(newUser.getEmail());
+            oldUser.setLogin(newUser.getLogin());
+            oldUser.setName(newUser.getName());
+            oldUser.setBirthday(newUser.getBirthday());
+            log.info("Изменен пользователь: {}", oldUser);
+            return oldUser;
+        }
+        log.warn("Пользователь с id = {} не найден", newUser.getId());
+        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
+    }
+
+    // вспомогательный метод для генерации идентификатора нового пользователя
+    private int getNextId() {
+        int currentMaxId = users.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    public static void validate(User user, boolean checkId) {
+        try {
+            if (checkId && user.getId() == null)
+                throw new ValidationException("Id должен быть указан");
+            if (StringUtils.containsWhitespace(user.getLogin()))
+                throw new ValidationException("Логин не может содержать пробелы");
+            if (StringUtils.isBlank(user.getName()))
+                user.setName(user.getLogin());
+        } catch (ValidationException e) {
+            log.warn("Ошибка валидации пользователя: {}", e.getMessage());
+            throw e;
+        }
+    }
+}
